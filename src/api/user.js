6932c392ed2e608ce8787a5e507bbba2cb57c11c
Deepavishali <deepavishalig@gmail.com>
2023-05-11 12:39:29 +0530

    updates

---
+++ src/api/user.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+export async function getAllUser(userId) {
+  return await axios.get(
+    `${BASE_URL}/crm/api/v1/users/${userId}`,
+    {
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    },
+    {
+      userId: localStorage.getItem("userId"),
+    }
+  );
+}
+
+export async function updateUserData(userId, selectedCurrUser) {
+  return await axios.put(
+    `${BASE_URL}/crm/api/v1/users/${userId} `,
+    selectedCurrUser,
+    {
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    },
+    {
+      userId: localStorage.getItem("userId"),
+    }
+  );
+}
