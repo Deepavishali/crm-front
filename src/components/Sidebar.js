3bb462a33fc8df9b9aeb3bb7cad2b214528fbfb7
Deepavishali <deepavishalig@gmail.com>
2023-05-11 12:52:20 +0530

    small changes in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,7 +14,7 @@ function Sidebar() {
             <CSidebarNav>
                 <CNavItem className='bg-dark d-flex'>
                     <i className='bi bi-bar-chart-fill text-white mx-3 my-2'></i>
-                    <h5 className='text-white fw-bolder mx-3 my-2'>TETHERX</h5>
+                    <h5 className='text-white fw-bolder mx-3 my-2'>CRM</h5>
                 </CNavItem>
                 <CNavTitle className='text-light fw-normal'>
                     A CRM app for all your needs..
